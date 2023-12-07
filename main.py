4652c3d8340bf2a2e09f23f2c18f599be28879f9
eidza <eidzaben24@gmail.com>
2023-12-07 14:19:31 -0600

    sockets?

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import socket
 from cryptography.fernet import Fernet
 
 # Generate a key from a passphrase (you might want to use a more secure method for real-world scenarios)
@@ -15,24 +16,37 @@ def decrypt_message(key, encrypted_message):
     cipher_suite = Fernet(key)
     return cipher_suite.decrypt(encrypted_message).decode()
 
-# GUI setup
 def send_message():
     message = entry.get()
     encrypted_msg = encrypt_message(shared_key, message)
-    # Display encrypted message in GUI
-    # You can send this encrypted_msg over the network
+    # Send the encrypted message over the network to the other user
+    client_socket.send(encrypted_msg)
 
 def receive_message():
-    # Simulating receiving an encrypted message from the network
-    encrypted_message = b'...'  # Replace with the received encrypted message
+    # Receive encrypted message from the other user
+    encrypted_message = client_socket.recv(1024)
     decrypted_msg = decrypt_message(shared_key, encrypted_message)
     # Display decrypted message in GUI
+    chat_box.insert(tk.END, decrypted_msg + '\n')
+
+def start_client():
+    global client_socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('127.0.0.1', 5555))  # Replace with the server's IP and port
+    receive_message()
+
+def start_server():
+    global client_socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('127.0.0.1', 5555))  # Replace with the server's IP and port
+    server_socket.listen(1)
+    client_socket, addr = server_socket.accept()
+    receive_message()
 
 # Replace 'passphrase' with the shared password between Alice and Bob
 passphrase = "sharedpassword"
 shared_key = generate_key(passphrase)
 
-# GUI initialization
 root = tk.Tk()
 root.title("Secure Chat")
 
@@ -48,7 +62,13 @@ entry.pack()
 send_button = tk.Button(frame, text="Send", command=send_message)
 send_button.pack()
 
-receive_button = tk.Button(frame, text="Receive", command=receive_message)
-receive_button.pack()
+chat_box = tk.Text(frame)
+chat_box.pack()
+
+start_server_button = tk.Button(frame, text="Start Server", command=start_server)
+start_server_button.pack()
+
+start_client_button = tk.Button(frame, text="Start Client", command=start_client)
+start_client_button.pack()
 
 root.mainloop()
